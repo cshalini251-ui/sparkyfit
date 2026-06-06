781d88d60f8ed72203805927312fa87d2478e1b1
cshalini251-ui <cshalini251@gmail.com>
2026-06-06 16:58:05 +0530

    Fix docs auto-imports for Docus theme

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,3 +1,5 @@
+import { resolve } from 'path'
+
 export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
   extends: ['@nuxt-themes/docus'],
@@ -10,5 +12,26 @@ export default defineNuxtConfig({
     //'@nuxtjs/plausible'
   ],
 
-  compatibilityDate: '2024-09-07'
+  compatibilityDate: '2024-09-07',
+
+  imports: {
+    imports: [
+      {
+        name: 'useDocus',
+        from: resolve(__dirname, 'node_modules/@nuxt-themes/docus/composables/useDocus.ts')
+      },
+      {
+        name: 'useMenu',
+        from: resolve(__dirname, 'node_modules/@nuxt-themes/docus/composables/useMenu.ts')
+      },
+      {
+        name: 'useScrollspy',
+        from: resolve(__dirname, 'node_modules/@nuxt-themes/docus/composables/useScrollspy.ts')
+      },
+      {
+        name: 'useDocSearch',
+        from: resolve(__dirname, 'node_modules/@nuxt-themes/docus/composables/useDocSearch.ts')
+      }
+    ]
+  }
 })
